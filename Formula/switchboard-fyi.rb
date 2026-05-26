00c0dc6a3337317a41650be8e808a5e7d0e63cbb
wd021 <dannywitters@gmail.com>
2026-05-26 10:33:57 +0100

    Update switchboard-fyi to 0.1.0

--- Formula/switchboard-fyi.rb
+++ Formula/switchboard-fyi.rb
@@ -2,7 +2,7 @@ class SwitchboardFyi < Formula
   desc "Local model-routing CLI for Codex and Claude Code"
   homepage "https://www.switchboard.fyi"
   url "https://registry.npmjs.org/switchboard-fyi/-/switchboard-fyi-0.1.0.tgz"
-  sha256 "2785ca0d1c0f70013dcfe5172849015ed12ab385088ee043982dc4092350c2c1"
+  sha256 "57e6bccd16d99b1db0e559e61777327d560307e66491dbb693e0961caad89152"
   license "MIT"
 
   depends_on "node"
@@ -11,6 +11,8 @@ class SwitchboardFyi < Formula
     system "npm", "install", *std_npm_args(prefix: libexec)
     bin.install_symlink libexec/"bin/switchboard"
     bin.install_symlink libexec/"bin/switchboard-fyi"
+    bin.install_symlink libexec/"bin/switchboard-gateway"
+    bin.install_symlink libexec/"bin/switchboard-inspector"
   end
 
   test do
